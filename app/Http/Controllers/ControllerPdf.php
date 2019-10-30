037a34c981333d81954cfad671ba70e748166922
Fran González <fran98@correo.ugr.es>
2019-10-30 18:13:39 +0100

    conexión base de datos #18

--- app/Http/Controllers/ControllerPdf.php
+++ app/Http/Controllers/ControllerPdf.php
@@ -3,22 +3,47 @@
 namespace App\Http\Controllers;
 
 use Laravel\Lumen\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
 use App\Pdf;
+use App\PrintCloud;
 
 class ControllerPdf extends BaseController
 {
-    public function verPdf(){
-        $pdf = new Pdf();
-        $pdf->setHtml("<!DOCTYPE html><html lang=\"es\"><head><title>Prueba</title></head><body>Hola Mundo!</body></html>");
-        $pdf->generarPdf();
-        $pdf->verPdf();
+    protected $pdf;
+
+    public function __construct() {
+        $this->pdf = new PrintCloud();
     }
 
-    public function descargarPdf(){
+    public function ver($id){
+        $model = Pdf::find($id);
+        $this->pdf->setDatos($model->datos);
+        $this->pdf->setPdfName($model->name.".pdf");
+        $this->pdf->generarHtml();
+        $this->pdf->generar();
+        $this->pdf->ver();
+    }
+
+    public function descargar($id){
+        $model = Pdf::find($id);
+        $this->pdf->setDatos($model->datos);
+        $this->pdf->setPdfName($model->name.".pdf");
+        $this->pdf->generarHtml();
+        $this->pdf->generar();
+        $this->pdf->descargar();
+    }
+
+    public function createPdf(Request $request){
         $pdf = new Pdf();
-        $pdf->setHtml("<!DOCTYPE html><html lang=\"es\"><head><title>Prueba</title></head><body>Hola Mundo!</body></html>");
-        $pdf->setPdfName('prueba.pdf');
-        $pdf->generarPdf();
-        $pdf->descargaPdf();
+        $pdf->nombre = $request->input('nombre');
+        // if (is_array($request->input('datos'))):
+            $pdf->datos = json_encode($request->input('datos'));
+        // else:
+        //     $pdf->datos = json_encode(Array($request->input('datos')));
+        // endif;
+        $pdf->save();
+        // $id = DB::table('pdf')->latest()->first();
+
+        return response()->json(['created'=>true]);
     }
 }
--- app/Pdf.php
+++ app/Pdf.php
@@ -1,60 +1,10 @@
 <?php
-
 namespace App;
-use Dompdf\Dompdf;
+use Illuminate\Database\Eloquent\Model;
 
-class Pdf{
-
-    protected $dompdf;
-    protected $pdfName;
-    protected $html;
-
-    public function __construct(){
-        $this->dompdf = new Dompdf();
-        $this->pdfName = "";
-        $this->html = "";
-    }
-
-    public function generarPdf(){
-
-        $this->dompdf->loadHtml($this->html);
-        
-        // (Optional) Setup the paper size and orientation
-        $this->dompdf->setPaper('A4');
-        
-        // Render the HTML as PDF
-        $this->dompdf->render();
-        
-    }
-    
-    public function descargaPdf(){
-        if ($this->pdfName != "")
-            $this->dompdf->stream($this->pdfName);        
-        else
-            echo 'No existe el pdf';
-    }
-    
-    public function verPdf(){
-        $this->dompdf->stream("MiDocumento.pdf", array("Attachment" => 0));
-    }
-
-    public function setHtml($string){
-        $this->html = $string;
-    }
-
-    public function getHtml(){
-        return $this->html;
-    }
-
-    public function setPdfName($name){
-        $this->pdfName = $name;
-    }
-
-    public function getPdfName(){
-        return $this->pdfName;
-    }
-
-    public function getDompdf(){
-        return $this->dompdf;
-    }
+class Pdf extends Model{
+  protected $table = 'pdf';
+  protected $primaryKey = 'id';
+  protected $fillable = ['id','nombre','datos'];
+  protected $hidden = ['created_at','updated_at'];
 }
