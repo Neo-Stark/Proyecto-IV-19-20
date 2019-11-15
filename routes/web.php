d778d3bc8f76648263f70ba326011149bd010882
Neo-Stark <fran.98.gg@gmail.com>
2019-11-15 13:06:49 +0000

    Apply fixes from StyleCI

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,8 @@ $router->get('/version', function () use ($router) {
     return $router->app->version();
 });
 $router->get('/', function () {
-    return response()->json(['status' => 'OK', 
-    'ejemplo'=>['ruta'=>'/documentos','valor'=>['1' => 'documento.pdf', '2' => 'prueba.pdf', '3' => 'lista.pdf']]]);
-
+    return response()->json(['status' => 'OK',
+    'ejemplo'=>['ruta'=>'/documentos', 'valor'=>['1' => 'documento.pdf', '2' => 'prueba.pdf', '3' => 'lista.pdf']], ]);
 });
 // $router->get('/pdf/{id}', 'ControllerPdf@ver');
 $router->get('/getPdf/{id}', 'ControllerPdf@descargar');
