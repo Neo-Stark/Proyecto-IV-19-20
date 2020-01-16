37c6dad42c629063dc0b066ecb7ee48f393e2284
Fran González <fran98@correo.ugr.es>
2020-01-16 22:02:26 +0100

    diseño tabla oscura close #49

--- resources/views/pdfView.php
+++ resources/views/pdfView.php
@@ -1,20 +1,30 @@
 <html>
-  <head>
-    <link rel="stylesheet" href="css/reset.css">
-    <!-- <link rel="stylesheet/less" type="text/css" href="css/pdf.less" /> -->
-    <link rel="stylesheet" href="css/pdf.css">
-    <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/3.9.0/less.min.js" ></script>
-  </head>
-    <body>
-      <header>
-        <!-- <img src="img/logo-ugr.png"> -->
-        <!-- <span class="separator"></span> -->
-        <!-- <span class="curso"> -->
-          <p>20<b>19</b></p>
-          <p><b>MEMORIA </b>curso académico</p>
-          <p>20<b>20</b></p>
-        <!-- </span> -->
-      </header>
-        <h1>Hello, <?php echo $name; ?></h1>
-    </body>
+<head>
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+  <?php
+  if (!is_null($datos)) {
+    $array = json_decode($datos);
+    echo '<table class="table table-dark table-striped table-borderless">';
+    echo '<thead class="thead-light"> <tr>';
+    foreach ($array[0] as $cabecera => $valor) {
+      echo '<th>' . $cabecera . '</th>';
+    }
+    echo '</tr> </thead>';
+    echo  '<tbody>';
+    foreach ($array as $tupla) {
+      echo '<tr>';
+      foreach ($tupla as $valor) {
+        echo '<td>' . $valor . '</td>';
+      }
+      echo '</tr>';
+    }
+    echo '</tbody> </table>';
+  }
+  ?>
+</body>
+
 </html>
